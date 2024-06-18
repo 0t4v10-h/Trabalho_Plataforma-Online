682f2140923d8c0db8c06e56adf35faf3b03b0e8
Otavio Henrique <otaviooliveirapt@hotmail.com>
2024-06-18 20:17:44 -0300

    Corrigindo o caminho do projeto

---
+++ br/edu/vianna/escola/model/Professor.java
@@ -0,0 +1,11 @@
+package br.edu.vianna.escola.model;
+
+public class Professor extends Usuario {
+    public Professor() {}
+
+    public Professor(int id, String nome, String email, String login, String senha) {
+        super(id, nome, email, login, senha);
+    }
+
+
+}
---
+++ br/edu/vianna/escola/model/escola/Avaliacao.java
@@ -0,0 +1,50 @@
+package br.edu.vianna.escola.model.escola;
+
+import br.edu.vianna.escola.model.Aluno;
+import br.edu.vianna.escola.model.Professor;
+
+public class Avaliacao {
+    private int id;
+    private Professor professor;
+    private Aluno aluno;
+    private int nota;
+
+    public Avaliacao(int id,Professor professor, Aluno aluno, int nota) {
+        this.id = id;
+        this.professor = professor;
+        this.aluno = aluno;
+        this.nota = nota;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Professor getProfessor() {
+        return professor;
+    }
+
+    public void setProfessor(Professor professor) {
+        this.professor = professor;
+    }
+
+    public Aluno getAluno() {
+        return aluno;
+    }
+
+    public void setAluno(Aluno aluno) {
+        this.aluno = aluno;
+    }
+
+    public int getNota() {
+        return nota;
+    }
+
+    public void setNota(int nota) {
+        this.nota = nota;
+    }
+}
